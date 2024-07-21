9eddff9967f2364ed438b7399558f28141451c68
ranjithswamy.shalli@gmail.com <!Jenukal6!>
2024-07-22 00:37:00 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { HomeModule } from "./home/home.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule, HomeModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}

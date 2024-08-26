3eb5ecea0da67700fa5b49b03bbce3633031c06a
ranjithswamy.shalli@gmail.com <!Jenukal6!>
2024-08-26 10:49:15 +0200

    adding form control

---
+++ src/app/services/postemail.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostemailService {
+
+  baseUrl: string = "https://formsubmit.co/ranjithswamy@gmail.com";
+
+  constructor(private http: HttpClient) { }
+
+  sendEmail(data: any): Observable<any> {
+    let body = {
+      name : "FormSubmit",
+      message : data
+    }
+
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json')
+              .set('Accept','application/json');
+
+    return this.http.post(this.baseUrl, JSON.stringify(body), {headers: headers});
+  }
+}
